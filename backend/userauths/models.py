ac4afaf1f615ef50d4979b10a769f132f406496d
elheit <meslamineelheit@gmail.com>
2024-07-28 22:44:13 +0100

    [REF] refactor the base User model and create a Profile model to split the work between Profile and User Models

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100, unique=True)
+    otp = models.CharField(max_length=30, unique=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self) :
+        return self.email
+    
+    def save(self, *args, **kwargs):
+        email_username , full_name = self.email.split("@")
+        if self.full_name == "" or self.full_name is None:
+            self.full_name == email_username
+        if self.username == "" or self.username is None:
+            self.username = email_username
+            
+        super(User, self).save()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="user_folder", default="default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return str(self.full_name) or str(self.user.full_name)
+    
+    def save(self, *args, **kwargs):
+        if self.full_name == "" or self.full_name is None:
+            self.full_name == self.user.username
+
+        super(Profile, self).save()
